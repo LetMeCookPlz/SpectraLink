00ca5ec7e092fab828cb74b03428441110b3669d
Anon <169835788+LetMeCookPlz@users.noreply.github.com>
2025-05-05 20:58:17 +0300

    Removed callbackUrl redirection from login page

--- app/login/page.js
+++ app/login/page.js
@@ -12,7 +12,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useRouter, useSearchParams } from "next/navigation"
+import { useRouter } from "next/navigation"
 import { AlertCircle } from 'lucide-react'
 import {
   Alert,
@@ -27,8 +27,6 @@ export default function LoginComponent() {
   const [error, setError] = useState("")
   const [isLoading, setIsLoading] = useState(false)
   const router = useRouter()
-  const searchParams = useSearchParams()
-  const callbackUrl = searchParams.get('callbackUrl') || '/plans'
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -39,14 +37,13 @@ export default function LoginComponent() {
       const result = await signIn('credentials', {
         redirect: false,
         email,
-        password,
-        callbackUrl
+        password
       })
 
       if (result?.error) {
         setError(result.error)
       } else {
-        router.push(callbackUrl)
+        router.push('/plans')
 				window.location.reload()
       }
     } catch (error) {
